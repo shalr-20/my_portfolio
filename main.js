92571fff427f90332b9f55b32dfa2c4220b5cd88
Shalini Roy <youremail@example.com>
2025-01-14 20:08:15 +0530

    ScrollReveal

--- main.js
+++ main.js
@@ -13,3 +13,39 @@ navLinks.addEventListener("click", (e)=>{
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
+
+const scrollRevealOption={
+    distance: "50px",
+    origin:"bottom",
+    duration:1000,
+};
+
+ScrollReveal().reveal(".header__image img",{
+    ...scrollRevealOption,
+    origin:"right",
+});
+
+ScrollReveal().reveal(".header__content h2",{
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header__content h1",{
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".header__content h3",{
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".header__content p",{
+    ...scrollRevealOption,
+    delay: 2000,
+});
+
+ScrollReveal().reveal(".header__btns",{
+    ...scrollRevealOption,
+    delay: 2500,
+});
